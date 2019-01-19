b89ac531c20d20adf8f6914ab01ab7f9b4a67c8c
MarcelKaemper <marcelkaemper862@gmail.com>
2019-01-20 00:06:30 +0100

    Added basic user registration

---
+++ public/javascripts/dbconn.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "db4free.net",
+	user: "creep2308",
+	password: "!Fw37*8FV^Ly",
+	database: "hckrgame"
+});
+
+module.exports = con;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
+// sql = require('../public/javascripts/dbconn.js')
+var con = require('../public/javascripts/dbconn.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
-router.get('/test', function(req, res, next) {
-  res.render('index', { title: 'Test!!!' });
+router.get('/signup', function(req, res, next) {
+	res.render('signup', { title: 'Sign up' });
+});
+
+router.post('/submit', function(req, res, next){
+	// res.render('signup', {title: req.body.email});
+	var mail = req.body.mail;
+	var name = req.body.username;
+	var password = req.body.password;
+	
+	var sql = "INSERT INTO logins(mail, name, password) VALUES('"+mail+"','"+name+"','"+password+"')";
+	con.query(sql, function(err, result){
+		if (err) throw err;
+	})
+	res.render('signup', {title: "test"});
 });
 
 module.exports = router;
