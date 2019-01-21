0e6638ded83878b9c4b4b520ea2369762c0f9a1e
MarcelKaemper <marcelkaemper862@gmail.com>
2019-01-21 21:39:15 +0100

    Added styles

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({secret: "secret", saveUninitialized: false, resave: false}));
 
 app.use('/', indexRouter);
