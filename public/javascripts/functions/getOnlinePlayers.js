7b7998bcd1f12ac83ab95222c83b84b72e89b96e
Exarous <csincay@yahoo.com>
2019-01-26 17:43:45 +0100

    Added online player list

--- public/javascripts/functions/getOnlinePlayers.js
+++ public/javascripts/functions/getOnlinePlayers.js
@@ -2,7 +2,7 @@ var query = require('../database/dbquery.js');
 
 function getOnlinePlayers(callback) {
     var players = [];
-    var sql = "SELECT name, loggedIn FROM logins;";
+    var sql = "SELECT name, loggedIn FROM logins WHERE loggedIn=true;";
 
     query(sql, function(results) {
         for(var i in results){
