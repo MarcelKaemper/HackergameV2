85452cea0305205b795c708c6a174937a4e12b7f
MarcelKaemper <marcelkaemper862@gmail.com>
2019-01-27 21:56:53 +0100

    The player who's logged in at the moment won't get added to the list; You can't transfer money to yourself

--- public/javascripts/functions/getAllPlayers.js
+++ public/javascripts/functions/getAllPlayers.js
@@ -1,10 +1,12 @@
 var query = require ('../database/dbquery.js');
 
-function getAllPlayers(callback){
+function getAllPlayers(ownUuid,callback){
 	query("SELECT uuid, name FROM logins", function(results){
 		var players = [];
 		for(var i in results){
-			players.push(results[i].name);
+			if(results[i].uuid != ownUuid){
+				players.push(results[i].name);
+			}
 		}
 		callback(players);
 	});
