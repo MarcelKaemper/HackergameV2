8eb231815351530a7cb8715faab694f9cef2656d
Exarous <csincay@yahoo.com>
2019-01-27 22:26:26 +0100

    Added console check command

--- public/javascripts/functions/console.js
+++ public/javascripts/functions/console.js
@@ -1,3 +1,7 @@
+var checkIP = require('./checkIP.js');
+var query = require('../database/dbquery.js');
+var checkAdmin = require('./checkAdmin.js');
+
 function consolecmd(req, cmd, callback) {
     if(req.session.command_log == null) {
         req.session.command_log = "";
@@ -19,6 +23,40 @@ function consolecmd(req, cmd, callback) {
             case "money":
                 req.session.command_log += req.session.name + "@" + req.session.ip + "> " + cmd + "\n";
                 callback();
+                break;
+            case "check":
+                req.session.command_log += req.session.name + "@" + req.session.ip + "> " + cmd + "\n";
+                checkAdmin(req.session.uuid, function(callb) {
+                    if(callb) {
+                        checkIP(command[1], function(call) {
+                            if(call) {
+                                var sql = "SELECT uuid, ip_address FROM userdata WHERE ip_address='" + command[1] + "';";
+                                
+                                query(sql, function(results) {
+                                    if(results <= 0) {
+                                        req.session.command_log += "Target not found!\n";
+                                        callback();
+                                    } else {
+                                        //req.session.command_log += "Target found!\n";
+                                        var sql1 = "SELECT name FROM logins WHERE uuid='" + results[0].uuid + "';";
+                                        query(sql1, function(results1) {
+                                            req.session.command_log += "Name: " + results1[0].name + "\n";
+                                            req.session.command_log += "IP: " + results[0].ip_address + "\n";
+                                            callback();
+                                        });
+                                    }
+                                });
+                            } else {
+                                req.session.command_log += "Unvalid ip format!\n";
+                                callback();
+                            }
+                        });
+                    } else {
+                        req.session.command_log += "You are not an admin!\n";
+                        callback();
+                    }
+                });
+                // callback();
                 break;
             default:
                 req.session.command_log += req.session.name + "@" + req.session.ip + "> " + cmd + "\n";
