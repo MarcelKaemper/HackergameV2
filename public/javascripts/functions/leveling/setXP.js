bf9e726e2ac9e5c76508ef95cdd4ca5c1a2127b0
Exarous <csincay@yahoo.com>
2019-01-29 17:41:55 +0100

    Fixed uuidName and setXP function

--- public/javascripts/functions/leveling/setXP.js
+++ public/javascripts/functions/leveling/setXP.js
@@ -2,12 +2,11 @@ var query = require('../../database/dbquery.js');
 var handleXP = require('./xpHandler.js');
 
 function setXP(uuid, amount, callback){
-	uuid = uuid;
 	var sql = "UPDATE levels SET xp='"+amount+"' WHERE uuid='"+uuid+"';";
 
-	query(sql, function(results){
-		query("SELECT level,xp FROM levels WHERE uuid='"+uuid+"';", function(results){
-			handleXP(uuid, results[0].level, results[0].xp, function(){
+	query(sql, function(results1){
+		query("SELECT level,xp FROM levels WHERE uuid='"+uuid+"';", function(results2){
+			handleXP(uuid, results2[0].level, results2[0].xp, function(){
 				// console.log("Needed for levelup: %d\nYou can reach level: %d", needed, nextlvl);
 				callback();
 			});
--- public/javascripts/functions/uuidName.js
+++ public/javascripts/functions/uuidName.js
@@ -1,18 +1,28 @@
 var query = require('../database/dbquery.js');
 
 function toName(uuid, callback) {
-    var sql = "SELECT name FROM logins WHERE uuid='" + uuid + "';";
-    query(sql, function(results) {
-        var name = results[0].name;
-        callback(name);
+    var sql1 = "SELECT name FROM logins WHERE uuid='" + uuid + "';";
+    var name = "";
+    query(sql1, function(results1) {
+        if(results1 <= 0) {
+            callback("");
+        } else {
+            name = results1[0].name;
+            callback(name);            
+        }
     });
 }
 
 function toUuid(name, callback) {
-    var sql = "SELECT uuid FROM logins WHERE name='" + name + "';";
-    query(sql, function(results) {
-        var uuid = results[0].uuid;
-        callback(uuid);
+    var sql2 = "SELECT uuid FROM logins WHERE name='" + name + "';";
+    var uuid = "";
+    query(sql2, function(results2) {
+        if(results2 <= 0) {
+            callback("");
+        } else {
+            uuid = results2[0].uuid;
+            callback(uuid);
+        }
     });
 }
 
