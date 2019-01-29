b81fb9314ef4c11885126bc047f2be1e72805659
MarcelKaemper <marcelkaemper862@gmail.com>
2019-01-29 18:34:20 +0100

    Added operations in setXP function

--- public/javascripts/functions/leveling/setXP.js
+++ public/javascripts/functions/leveling/setXP.js
@@ -1,14 +1,50 @@
-	var query = require('../../database/dbquery.js');
+var query = require('../../database/dbquery.js');
 var handleXP = require('./xpHandler.js');
+var uuidName = require('../uuidName.js');
 
-function setXP(uuid, amount, callback){
-	var sql = "UPDATE levels SET xp="+amount+" WHERE uuid='"+uuid+"';";
+function setXP(uuid, amount, operation, callback){
+	var sql;
 
-	query(sql, function(results){
-		handleXP(uuid, amount, function(){
+	switch(operation){
+		case "give":
+			sql = "UPDATE levels SET xp=xp+"+amount+" WHERE uuid='"+uuid+"';";
+			query(sql, function(results){
+				handleXP(uuid, amount, function(){
+					callback();
+				});
+			});
+			break;
+
+		case "take":
+			sql = "UPDATE levels SET xp=xp-"+amount+" WHERE uuid='"+uuid+"';";
+			query(sql, function(results){
+				handleXP(uuid, amount, function(){
+					callback();
+				});
+			});
+			break;
+
+		case "set":	
+			sql = "UPDATE levels SET xp="+amount+" WHERE uuid='"+uuid+"';";
+			query(sql, function(results){
+				handleXP(uuid, amount, function(){
+					callback();
+				});
+			});
+			break;
+
+		case "show":	
+			sql = "SELECT level, xp FROM levels WHERE uuid='"+uuid+"';";
+			query(sql, function(results){
+				uuidName.toName(results[0].uuid, function(name){
+					callback(results[0].level, results[0].xp, name);
+				});
+			});
+			break;
+
+		default:
 			callback();
-		});
-	});
+			break;
 };
 
 
