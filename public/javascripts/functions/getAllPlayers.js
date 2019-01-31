01375f3a400ec7e50930e18149ec790d285fd53c
Exarous <csincay@yahoo.com>
2019-01-31 21:48:36 +0100

    Fixed receive player functions

--- public/javascripts/functions/getAllPlayers.js
+++ public/javascripts/functions/getAllPlayers.js
@@ -1,14 +1,16 @@
 var query = require ('../database/dbquery.js');
 
-async function getAllPlayers(ownUuid,callback){
-	var results = await query("SELECT uuid, name FROM logins;");
-	var players = [];
-	for(var i in results){
-		if(results[i].uuid != ownUuid){
-			players.push(results[i].name);
+function getAllPlayers(ownUuid) {
+	return new Promise(async function(resolve, reject) {
+		var results = await query("SELECT uuid, name FROM logins;");
+		var players = [];
+		for(var i in results){
+			if(results[i].uuid != ownUuid){
+				players.push(results[i].name);
+			}
 		}
-	}
-	callback(players);
+		resolve(players);
+	});
 }
 
 module.exports = getAllPlayers;
--- public/javascripts/functions/getOnlinePlayers.js
+++ public/javascripts/functions/getOnlinePlayers.js
@@ -1,14 +1,16 @@
 var query = require('../database/dbquery.js');
 
-async function getOnlinePlayers(callback) {
-    var players = [];
-    var sql = "SELECT name, loggedIn FROM logins WHERE loggedIn=true;";
+function getOnlinePlayers() {
+    return new Promise(async function(resolve, reject) {
+        var players = [];
+        var sql = "SELECT name, loggedIn FROM logins WHERE loggedIn=true;";
 
-    var results = await query(sql); 
-    for(var i in results){
-	players.push(results[i].name);
-    }
-    callback(players);
+        var results = await query(sql); 
+        for(var i in results){
+            players.push(results[i].name);
+        }
+        resolve(players);
+    });
 }
 
 module.exports = getOnlinePlayers;
