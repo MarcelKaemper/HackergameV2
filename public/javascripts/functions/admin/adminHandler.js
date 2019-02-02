9c0f53694a9d254ad39cec7045e7d130cd9532ed
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-02 02:01:40 +0100

    Implemented functions to set, give and take xp via adminarea

--- public/javascripts/functions/admin/adminHandler.js
+++ public/javascripts/functions/admin/adminHandler.js
@@ -1,16 +1,25 @@
 var query = require('../../database/dbquery.js');
 var toUuid = require('../uuidName.js');
 var delUser = require('./delUser.js');
+var setxp = require('../leveling/setXP.js');
 
 
-function adminHandler(operation, username){
+function adminHandler(operation, username, additional){
 	return new Promise(async function(resolve, reject){
 		var uuid = await toUuid.toUuid(username); 
 		switch(operation){
-			case "Delete":
+			case "delete":
 				await delUser(uuid);
 				break;
-
+			case "setxp":
+				await setxp(uuid, additional, "set");
+				break;
+			case "givexp":
+				await setxp(uuid, additional, "give");
+				break;
+			case "takexp":
+				await setxp(uuid, additional, "take");
+				break;
 			default:
 				break;
 		}
