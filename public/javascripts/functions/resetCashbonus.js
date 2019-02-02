08791fefd9bede793153f80ba4670028188997bd
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-03 00:53:33 +0100

    Let's you claim your bonus again after 30 minutes

---
+++ public/javascripts/functions/resetCashbonus.js
@@ -0,0 +1,18 @@
+var query = require('../database/dbquery.js');
+var moment = require('moment'); 
+
+function resetCashbonus(){
+    return new Promise(async function(resolve, reject) {
+        var results = await query("SELECT * FROM cashbonus");
+    	for(var i in results) {
+				if(moment().diff(results[i].lastClaim, "minutes")>=30) {
+                    console.log("object");
+					sql = "UPDATE cashbonus SET claimed=false where uuid='"+results[i].uuid+"';";
+                    await query(sql);
+                };
+        };
+        resolve();
+    });
+}
+
+module.exports = resetCashbonus;
--- public/javascripts/functions/stdCall.js
+++ public/javascripts/functions/stdCall.js
@@ -1,12 +1,14 @@
 var logoutInactive = require('./logoutInactivePlayers.js');
 var sessionReload = require('./loadSessionVars.js');
 var writeActivity = require('./writeActivity.js');
+var resetCashbonus = require('./resetCashbonus.js');
 
 async function stdCall(req) {
 	return new Promise(async function(resolve, reject) {
 		await logoutInactive(req.session.loggedIn);
 		await sessionReload(req);
 		await writeActivity(req.session.uuid);
+		await resetCashbonus();
 		resolve();
 	});
 }
