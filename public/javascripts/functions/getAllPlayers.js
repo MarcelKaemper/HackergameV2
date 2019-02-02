f71638d146af49baab0ba524fbc303b0cfd4d8b2
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-02 17:41:10 +0100

    Added operators to getAllPlayers function.

--- public/javascripts/functions/getAllPlayers.js
+++ public/javascripts/functions/getAllPlayers.js
@@ -1,15 +1,23 @@
 var query = require ('../database/dbquery.js');
 var checkAdmin = require ('../functions/checkAdmin.js');
 
-function getAllPlayers(ownUuid) {
+function getAllPlayers(ownUuid, operator) {
 	return new Promise(async function(resolve, reject) {
 		var results = await query("SELECT uuid, name, displayName FROM logins;");
 		var players = [];
 		for(var i in results) {
-			if(results[i].uuid != ownUuid) {
-				if(!await checkAdmin(results[i].uuid)) {
+			switch(operator){
+				case "everyone":
 					players.push(results[i].displayName);
+					break;
+				//Everyone but yourself
+				case "everyoneButYou":
+					if(results[i].uuid != ownUuid) {
+						players.push(results[i].displayName);
+					break;
 				}
+				default:
+					break;
 			}
 		}
 		resolve(players);
