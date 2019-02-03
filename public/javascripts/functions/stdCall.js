c3047887b118937534c6895f38f2470407ba6b73
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-03 21:32:03 +0100

    Removed unused imports

--- public/javascripts/functions/stdCall.js
+++ public/javascripts/functions/stdCall.js
@@ -1,7 +1,5 @@
-var logoutInactive = require('./logoutInactivePlayers.js');
 var sessionReload = require('./loadSessionVars.js');
 var writeActivity = require('./writeActivity.js');
-var resetCashbonus = require('./resetCashbonus.js');
 
 async function stdCall(req) {
 	return new Promise(async function(resolve, reject) {
