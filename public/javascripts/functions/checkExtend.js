c2f99ca33dc033bab1392a9d8ab1672553954793
Exarous <csincay@yahoo.com>
2019-02-03 01:16:18 +0100

    Extended checkExtend function; Removed console logs

--- public/javascripts/functions/checkExtend.js
+++ public/javascripts/functions/checkExtend.js
@@ -4,13 +4,15 @@ function IP(ipaddress) {
     return new Promise(async function(resolve, reject) {
         var sql1 = "SELECT ip_address FROM userdata WHERE ip_address='" + ipaddress + "';";
         var sql2 = "SELECT ip_address FROM bankAccounts WHERE ip_address='" + ipaddress + "';";
+        var sql3 = "SELECT ip_address FROM server WHERE ip_address='" + ipaddress + "';";
 
         var results1 = await query(sql1);
         var results2 = await query(sql2);
-        if(results1 <= 0 || results2 <= 0) {
-            resolve(false);
-        } else {
+        var results3 = await query(sql3);
+        if(results1.length >= 1 || results2.length >= 1 || results3.length >= 1) {
             resolve(true);
+        } else {
+            resolve(false);
         }
     });
 }
@@ -19,13 +21,15 @@ function UUID(uuid) {
     return new Promise(async function(resolve, reject) {
         var sql1 = "SELECT uuid FROM userdata WHERE uuid='" + uuid + "';";
         var sql2 = "SELECT uuid FROM bankAccounts WHERE uuid='" + uuid + "';";
+        var sql3 = "SELECT uuid FROM server WHERE uuid='" + uuid + "';";
 
         var results1 = await query(sql1);
         var results2 = await query(sql2);
-        if(results1 <= 0 || results2 <= 0) {
-            resolve(false);
-        } else {
+        var results3 = await query(sql3);
+        if(results1.length >= 1 || results2.length >= 1 || results3.length >= 1) {
             resolve(true);
+        } else {
+            resolve(false);
         }
     });
 }
