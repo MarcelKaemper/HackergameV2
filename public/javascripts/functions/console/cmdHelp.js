519fc1f24e606c065a5a216cb78d6d22da729647
Exarous <csincay@yahoo.com>
2019-02-06 21:35:52 +0100

    Added command server to command help

--- public/javascripts/functions/console/cmdHelp.js
+++ public/javascripts/functions/console/cmdHelp.js
@@ -2,7 +2,8 @@ function cmdHelp(req, cmd, command, callback) {
     req.session.command_log += "help ~ Shows this help page\n"
                             + "clear ~ Clears the commandlog\n"
                             + "scan [ip, (network)] <target> ~ Scan a target\n"
-                            + "money [tr, transfer] <target> ~ Steal money from a target\n";
+                            + "money [tr, transfer] <target> ~ Steal money from a target\n"
+                            + "server [connect] <target> <password> ~ Connect to a server\n";
     callback();
 }
 
