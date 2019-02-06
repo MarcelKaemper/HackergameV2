404922b482065051ed1e8a0df5cab6544267b94c
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-06 20:59:55 +0100

    Changing money when buying stocks

--- public/javascripts/functions/stocks/buyStock.js
+++ public/javascripts/functions/stocks/buyStock.js
@@ -1,4 +1,5 @@
 var query = require('../../database/dbquery.js');
+var changeMoney = require('../changeMoney.js');
 
 const buyStock = async(uuid, name, price) => {
     var ownedStocks = await query("SELECT stocks FROM stocks WHERE uuid='"+uuid+"';");
@@ -8,10 +9,12 @@ const buyStock = async(uuid, name, price) => {
         ownedStocks = Object.assign(JSON.parse('{"'+name+'": {"price":'+price+',"count":1}}'), ownedStocks);
         console.log(ownedStocks);
         await query("UPDATE stocks SET stocks='"+JSON.stringify(ownedStocks)+"' WHERE uuid='"+uuid+"';");
+        await changeMoney(uuid, price, "take");
     }else{
         ownedStocks[name].count += 1;
         ownedStocks[name].price += price;
         await query("UPDATE stocks SET stocks='"+JSON.stringify(ownedStocks)+"' WHERE uuid='"+uuid+"';");
+        await changeMoney(uuid, price, "take");
     }
 }
 
