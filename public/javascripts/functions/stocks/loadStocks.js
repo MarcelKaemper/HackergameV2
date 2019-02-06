c9c4fc667a09db60df51b53aaca8aec450000bca
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-06 16:52:22 +0100

    Added function to get all stocks that someone owns

---
+++ public/javascripts/functions/stocks/loadStocks.js
@@ -0,0 +1,10 @@
+var query = require('../../database/dbquery.js');
+
+const loadStock = (uuid) => {
+    return new Promise(async(resolve, reject) => {
+        let results = await query("SELECT * FROM stocks WHERE uuid='"+uuid+"';");
+        resolve(JSON.parse(results[0].stocks));
+    });
+}
+
+module.exports = loadStock;
