ddbe726620d5dcef6028a3b8ab20f91bf722ea69
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-07 12:49:25 +0100

    You can now enter the amount of shares you wanna buy

--- public/javascripts/functions/stocks/buyStock.js
+++ public/javascripts/functions/stocks/buyStock.js
@@ -1,20 +1,22 @@
 var query = require('../../database/dbquery.js');
 var changeMoney = require('../changeMoney.js');
 
-const buyStock = async(uuid, name, price) => {
+const buyStock = async(uuid, name, price, count) => {
     var ownedStocks = await query("SELECT stocks FROM stocks WHERE uuid='"+uuid+"';");
     ownedStocks= JSON.parse(ownedStocks[0].stocks);
     if(!ownedStocks.hasOwnProperty(name)){
         console.log(ownedStocks);
-        ownedStocks = Object.assign(JSON.parse('{"'+name+'": {"price":'+price+',"count":1}}'), ownedStocks);
+        ownedStocks = Object.assign(JSON.parse('{"'+name+'": {"price":'+price*count+',"count":'+count+'}}'), ownedStocks);
         console.log(ownedStocks);
-        await query("UPDATE stocks SET stocks='"+JSON.stringify(ownedStocks)+"' WHERE uuid='"+uuid+"';");
-        await changeMoney(uuid, price, "take");
+        if(await changeMoney(uuid, price*count, "take")){
+            await query("UPDATE stocks SET stocks='"+JSON.stringify(ownedStocks)+"' WHERE uuid='"+uuid+"';");
+        }
     }else{
-        ownedStocks[name].count += 1;
-        ownedStocks[name].price += price;
-        await query("UPDATE stocks SET stocks='"+JSON.stringify(ownedStocks)+"' WHERE uuid='"+uuid+"';");
-        await changeMoney(uuid, price, "take");
+        ownedStocks[name].count += count;
+        ownedStocks[name].price += count*price;
+        if(await changeMoney(uuid, price*count, "take")){
+            await query("UPDATE stocks SET stocks='"+JSON.stringify(ownedStocks)+"' WHERE uuid='"+uuid+"';");
+        }
     }
 }
 
