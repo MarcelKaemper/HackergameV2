c1b109e7279e67fd5db72ab9c924d4269d39d56c
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-07 12:49:44 +0100

    Changemoney checks if the given user has enough money

--- public/javascripts/functions/changeMoney.js
+++ public/javascripts/functions/changeMoney.js
@@ -9,9 +9,16 @@ function changeMoney(uuid, amount, operation) {
 			await query(sql);
 			resolve();
 		} else if(operation == "take") {
-			sql = "UPDATE money SET money=money-"+amount+" WHERE uuid='"+uuid+"';";
-			await query(sql);
-			resolve();
+			let currentMoney = await query("SELECT money FROM money WHERE uuid='"+uuid+"';");
+			if(currentMoney[0].money>amount){
+				sql = "UPDATE money SET money=money-"+amount+" WHERE uuid='"+uuid+"';";
+				await query(sql);
+				//successful
+				resolve(true);
+			}else{
+				// Not successful
+				resolve(false);
+			}
 		 }else if(operation == "set") {
 			sql = "UPDATE money SET money="+amount+" WHERE uuid='"+uuid+"';";
 			await query(sql);
