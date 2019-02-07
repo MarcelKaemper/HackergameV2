4b219d7e37a10633e2638948a65b68bf9de35a84
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-07 12:21:11 +0100

    Added function to sell stocks

--- public/javascripts/functions/stocks/sellStock.js
+++ public/javascripts/functions/stocks/sellStock.js
@@ -1,4 +1,16 @@
-const sellStock = (uuid, name) => {
+var changeMoney = require('../changeMoney.js');
+var loadStock = require('./loadStocks.js');
+var query = require('../../database/dbquery.js');
+
+const sellStock = (uuid, symbol, count, worth) => {
+    return new Promise(async (resolve, reject) => {
+        let stocks = await loadStock(uuid);
+        delete stocks[symbol];
+
+        changeMoney(uuid, worth, "give");
+        await query("UPDATE stocks SET stocks='"+JSON.stringify(stocks)+"' WHERE uuid='"+uuid+"';");
+        resolve();
+    });
 
 }
 
