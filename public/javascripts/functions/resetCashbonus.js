6ea524e69be8625831dbd6423a65b8156d0a7118
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-09 12:28:11 +0100

    Cashbonus claimable every 2 hours

--- public/javascripts/functions/resetCashbonus.js
+++ public/javascripts/functions/resetCashbonus.js
@@ -5,7 +5,7 @@ function resetCashbonus() {
     return new Promise(async function(resolve, reject) {
         var results = await query("SELECT * FROM cashbonus");
     	for(var i in results) {
-				if(moment().diff(results[i].lastClaim, "minutes")>=30) {
+				if(moment().diff(results[i].lastClaim, "minutes")>=120) {
 					sql = "UPDATE cashbonus SET claimed=false where uuid='"+results[i].uuid+"';";
                     await query(sql);
                 };
