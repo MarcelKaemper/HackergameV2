c9d801bc66bf8b132aadff572d5d873d76c2c297
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-10 13:54:32 +0100

    Added clan router

--- routes/clan.js
+++ routes/clan.js
@@ -2,10 +2,18 @@ var express = require('express');
 var router = express.Router();
 var stdParameter = require('../public/javascripts/functions/stdParameter.js');
 var stdCall = require('../public/javascripts/functions/stdCall.js');
+var loadClans = require('../public/javascripts/functions/clans/loadClans.js');
+var createClan = require('../public/javascripts/functions/clans/createClan.js');
 
-router.get('/', (req, res, next) => {
+router.get('/', async(req, res, next) => {
     stdCall(req);
-    res.render('clan', stdParameter(req, 'Clans', {clans:{"cool":{members:12}}}));
+    let clans = await loadClans();
+    res.render('clan', stdParameter(req, 'Clans', {clans:clans}));
+});
+
+router.post('/createClan', async(req, res, next) => {
+    await createClan(req.session.uuid, req.body.name, req.body.maxMembers, req.session.name);
+    res.redirect('/clan');
 });
 
 module.exports = router;
