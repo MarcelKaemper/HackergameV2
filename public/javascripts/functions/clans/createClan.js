c9a1eb44940a89a755bc40f591c05ab93a6f2396
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-10 14:54:35 +0100

    Checks if user is able to join the created clan, if not, the clan gets deleted

--- public/javascripts/functions/clans/createClan.js
+++ public/javascripts/functions/clans/createClan.js
@@ -1,6 +1,7 @@
 var query = require('../../database/dbquery.js');
 var loadClans = require('./loadClans.js');
 var generator = require('../generator.js');
+var joinClan = require('./joinClan.js');
 
 const createClan = (uuid, name, maxMembers, username) => {
     return new Promise(async(resolve, reject) => {
@@ -13,9 +14,11 @@ const createClan = (uuid, name, maxMembers, username) => {
             }
         }
         let clan_uuid = await generator.genUUID();
-        let values = "('"+clan_uuid+"','"+name+"', 1, "+maxMembers+",' {[\""+username+"\"],[\""+uuid+"\"]}');";
-
+        let values = "('"+clan_uuid+"','"+name+"', 0,"+maxMembers+",' {\"names\":[],\"uuids\":[]}');";
         await query("INSERT INTO clans(uuid,name,memberCount,maxMembers,members) VALUES "+values);
+        if(!await joinClan(clan_uuid, name, uuid, username, 0, maxMembers)){
+            await query("DELETE * FROM clans WHERE uuid='"+clan_uuid+"';");
+        }
         resolve(true);
     });
 }
