e3b8d474d558ca43f6e646834d2aad10fcff70d6
MarcelKaemper <marcelkaemper862@gmail.com>
2019-02-10 14:44:02 +0100

    Added join clan route

--- routes/clan.js
+++ routes/clan.js
@@ -4,11 +4,17 @@ var stdParameter = require('../public/javascripts/functions/stdParameter.js');
 var stdCall = require('../public/javascripts/functions/stdCall.js');
 var loadClans = require('../public/javascripts/functions/clans/loadClans.js');
 var createClan = require('../public/javascripts/functions/clans/createClan.js');
+var joinClan = require('../public/javascripts/functions/clans/joinClan');
 
 router.get('/', async(req, res, next) => {
     stdCall(req);
     let clans = await loadClans();
     res.render('clan', stdParameter(req, 'Clans', {clans:clans}));
+});
+
+router.post('/joinClan', async(req, res, next) => {
+    await joinClan(req.body.uuid, req.body.name, req.session.uuid, req.session.name, req.body.memberCount, req.body.maxMembers);
+    res.redirect('/clan');
 });
 
 router.post('/createClan', async(req, res, next) => {
