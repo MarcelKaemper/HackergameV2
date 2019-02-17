ea1f2882cef9aafac4b5c8f0413eb35969c38ce8
Exarous <csincay@yahoo.com>
2019-02-17 21:35:16 +0100

    Added new converting functions

--- public/javascripts/functions/uuidName.js
+++ public/javascripts/functions/uuidName.js
@@ -28,7 +28,67 @@ function toUuid(name) {
     });
 }
 
+function toSrvUuid(ipaddress) {
+    return new Promise(async function(resolve, reject) {
+        var sql3 = "SELECT uuid FROM server WHERE ip_address='" + ipaddress + "';";
+        var uuid2 = "";
+        var results3 = await query(sql3);
+        if(results3 <= 0) {
+            resolve("");
+        } else {
+            uuid2 = results3[0].uuid;
+            resolve(uuid2);
+        }
+    });
+}
+
+function toSrvIP(uuid) {
+    return new Promise(async function(resolve, reject) {
+        var sql4 = "SELECT ip_address FROM server WHERE uuid='" + uuid + "';";
+        var ipaddress = "";
+        var results4 = await query(sql4);
+        if(results4 <= 0) {
+            resolve("");
+        } else {
+            ipaddress = results4[0].ip_address;
+            resolve(ipaddress);
+        }
+    });
+}
+
+function toItemUuid(name) {
+    return new Promise(async function(resolve, reject) {
+        var sql5 = "SELECT uuid FROM shop WHERE itemName='" + name + "';";
+        var uuid3 = "";
+        var results5 = await query(sql5);
+        if(results5 <= 0) {
+            resolve("");
+        } else {
+            uuid3 = results5[0].uuid;
+            resolve(uuid3);
+        }
+    });
+}
+
+function toItemName(uuid) {
+    return new Promise(async function(resolve, reject) {
+        var sql6 = "SELECT itemName FROM shop WHERE uuid='" + uuid + "';";
+        var name2 = "";
+        var results6 = await query(sql6);
+        if(results6 <= 0) {
+            resolve("");
+        } else {
+            name2 = results6[0].itemName;
+            resolve(name2);
+        }
+    });
+}
+
 module.exports = {
     toName,
-    toUuid
+    toUuid,
+    toSrvUuid,
+    toSrvIP,
+    toItemUuid,
+    toItemName
 }
