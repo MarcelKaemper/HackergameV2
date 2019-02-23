484c0a46983df432779db3406d78f8813ac46586
Exarous <csincay@yahoo.com>
2019-02-23 23:43:52 +0100

    Removed console logs

--- public/javascripts/functions/inventory/installSrvItem.js
+++ public/javascripts/functions/inventory/installSrvItem.js
@@ -13,23 +13,17 @@ function installSrvItem(req) {
         var sql = "SELECT * FROM shop WHERE uuid='" + itemuuid + "';";
 
         if(itemuuid != "" && itemuuid != null && itemuuid != undefined) {
-            console.log("saas");
             var results = await query(sql);
             if(results.length > 0) {
                 var itemcategory = results[0].category;
                 var itemprice = results[0].itemPrice;
                 var newrevenue = parseInt(itemprice * (-1));
                 var checkitem = await checkSrvItem(srvuuid, itemuuid);
-                console.log("soos");
 
                 if(itemcategory == category && !checkitem) {
-                    console.log("sees");
                     await addServerRevenue(srvuuid, newrevenue);
-                    console.log("siis");
                     await delInventory(usruuid, itemuuid);
-                    console.log("suus");
                     await addSrvInventory(srvuuid, itemuuid);
-                    console.log("sneees");
                     resolve(true);
                 } else {
                     resolve(false);
