ca93e2f6a2b5eabf27ce46f8e3c6b06e890d37a8
Marcel <marcelkaemper862@gmail.com>
2019-04-26 19:28:49 +0200

    Formatted document

--- routes/clan.js
+++ routes/clan.js
@@ -8,35 +8,35 @@ var joinClan = require('../public/javascripts/functions/clans/joinClan');
 var clanInfo = require('../public/javascripts/functions/clans/clanInfo');
 var leaveClan = require('../public/javascripts/functions/clans/leaveClan.js');
 
-router.get('/', async(req, res, next) => {
+router.get('/', async (req, res, next) => {
     await stdCall(req);
     let clans = await loadClans();
     let currentClan = req.session.clan;
     let currentMembers = await clanInfo(currentClan)
-    res.render('clan/clan', stdParameter(req, 'Clans', {currentClan: currentClan, members: currentMembers, clans:clans}));
+    res.render('clan/clan', stdParameter(req, 'Clans', { currentClan: currentClan, members: currentMembers, clans: clans }));
 });
 
-router.post('/joinClan', async(req, res, next) => {
+router.post('/joinClan', async (req, res, next) => {
     await joinClan(req.body.uuid, req.body.name, req.session.uuid, req.session.displayName, req.body.memberCount, req.body.maxMembers);
     res.redirect('/clan');
 });
 
-router.post('/createClan', async(req, res, next) => {
+router.post('/createClan', async (req, res, next) => {
     await createClan(req.session.uuid, req.body.name, req.body.maxMembers, req.session.displayName);
     res.redirect('/clan');
 });
 
-router.get('/info', async(req, res, next) => {
+router.get('/info', async (req, res, next) => {
     await stdCall(req);
     let clan = req.query.clan;
-    if(clan == undefined){
+    if (clan == undefined) {
         clan = req.session.clan;
     }
     let members = await clanInfo(clan);
-    res.render('clan/info', stdParameter(req, clan, {members: members}));
+    res.render('clan/info', stdParameter(req, clan, { members: members }));
 })
 
-router.get('/leaveclan', async(req, res, next) => {
+router.get('/leaveclan', async (req, res, next) => {
     await leaveClan(req);
     res.redirect('/clan');
 });
--- routes/index.js
+++ routes/index.js
@@ -22,117 +22,117 @@ var getTopPlayers = require('../public/javascripts/functions/getTopPlayers.js');
 
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
 	await stdCall(req);
-	res.render('index', stdParameter(req, 'Hackergame', {onlinePlayers: await getOnlinePlayers(), topPlayers: await getTopPlayers()}));
+	res.render('index', stdParameter(req, 'Hackergame', { onlinePlayers: await getOnlinePlayers(), topPlayers: await getTopPlayers() }));
 });
 
-router.get('/signup', async function(req, res, next) {
+router.get('/signup', async function (req, res, next) {
 	await stdCall(req);
-	res.render('signup', stdParameter(req, 'Sign up', {message: req.query.error}));
+	res.render('signup', stdParameter(req, 'Sign up', { message: req.query.error }));
 });
 
-router.get('/login', async function(req, res, next) {
+router.get('/login', async function (req, res, next) {
 	await stdCall(req);
 	res.render('login', stdParameter(req, 'Login', {}));
 });
 
-router.get('/bank', async function(req, res, next) {
+router.get('/bank', async function (req, res, next) {
 	await stdCall(req);
 	let players = await getAllPlayers(req.session.uuid, "everyoneButYou");
-	res.render('bank', stdParameter(req, 'Bank', {money: req.session.money, players: players}));
+	res.render('bank', stdParameter(req, 'Bank', { money: req.session.money, players: players }));
 });
 
-router.post('/bank', async function(req, res, next) {
+router.post('/bank', async function (req, res, next) {
 	await stdCall(req);
 	await transferMoney(req);
 	res.redirect('/bank');
 });
 
-router.get('/admin', async function(req, res, next) {
+router.get('/admin', async function (req, res, next) {
 	await stdCall(req);
-	res.render('admin', stdParameter(req, 'Adminarea', {players: await getAllPlayers(req.session.uuid, "everyone")}));
+	res.render('admin', stdParameter(req, 'Adminarea', { players: await getAllPlayers(req.session.uuid, "everyone") }));
 });
 
-router.post('/deposit', async function(req, res, next) {
+router.post('/deposit', async function (req, res, next) {
 	await stdCall(req);
 	await changeMoney(req.session.uuid, req.body.amount, "give");
 	res.redirect('/');
 });
 
-router.get('/profile', async function(req, res, next) {
+router.get('/profile', async function (req, res, next) {
 	await stdCall(req);
-	res.render('profile', stdParameter(req, 'Profile', {user: await getUserInfo(req)}));
+	res.render('profile', stdParameter(req, 'Profile', { user: await getUserInfo(req) }));
 });
 
-router.get('/logout', async function(req, res, next) {
+router.get('/logout', async function (req, res, next) {
 	await setLoggedIn(false, req.session.uuid);
 	req.session.destroy();
 	res.redirect('/');
 });
 
-router.get('/console', async function(req, res, next) {
+router.get('/console', async function (req, res, next) {
 	await stdCall(req);
-	res.render('console', stdParameter(req, 'Console', {message: req.session.command_log}));
+	res.render('console', stdParameter(req, 'Console', { message: req.session.command_log }));
 });
 
-router.get('/shop', async function(req, res, next) {
+router.get('/shop', async function (req, res, next) {
 	await stdCall(req);
-	res.render('shop', stdParameter(req, 'Shop', {shoplist: await listShop(), message: req.query.error}));
+	res.render('shop', stdParameter(req, 'Shop', { shoplist: await listShop(), message: req.query.error }));
 });
 
-router.post('/buyshop', async function(req, res, next) {
+router.post('/buyshop', async function (req, res, next) {
 	var success = await buyShop(req);
-	if(success) {
+	if (success) {
 		res.redirect('/shop');
 	} else {
 		res.redirect('/shop?error=buyFailed');
 	}
 });
 
-router.post('/sellshop', async function(req, res, next) {
+router.post('/sellshop', async function (req, res, next) {
 	await sellShop(req);
 	res.redirect('/inventory');
 });
 
-router.get('/inventory', async function(req, res, next) {
+router.get('/inventory', async function (req, res, next) {
 	var getinventory = await loadInventory(req.session.uuid);
 	var inventory = await getItemName(getinventory);
-	res.render('inventory', stdParameter(req, 'Inventory', {inventory: inventory}));
+	res.render('inventory', stdParameter(req, 'Inventory', { inventory: inventory }));
 });
 
-router.get('/cashbonus', async function(req, res, next) {
+router.get('/cashbonus', async function (req, res, next) {
 	await cashbonus(req.session.uuid);
 	res.redirect('/');
 });
 
-router.post('/admin', async function(req, res, next) {
-	if(req.body.confirm) {
+router.post('/admin', async function (req, res, next) {
+	if (req.body.confirm) {
 		await adminAreaHandler(req.body.operation, req.body.user, req.body.additional);
 	}
 	res.redirect('/admin');
 });
 
-router.post('/console', async function(req, res, next) {
+router.post('/console', async function (req, res, next) {
 	await consolecmd(req, req.body.command);
 	//res.render('console', {loggedIn: req.session.loggedIn, message: message});
 	res.redirect('/console');
 });
 
-router.post('/login', async function(req,res,next) {
+router.post('/login', async function (req, res, next) {
 	var success = await login(req, req.body.login, req.body.password);
 
-	if(success) {
+	if (success) {
 		res.redirect('/');
 	} else {
 		res.redirect('/login?error=loginFailed');
 	}
 });
 
-router.post('/signup', async function(req, res, next) {
+router.post('/signup', async function (req, res, next) {
 	var success = await signup(req, req.body.mail, req.body.username, req.body.password, req.body.confirmPassword);
 
-	if(success) {
+	if (success) {
 		res.redirect('login');
 	} else {
 		res.redirect('signup?error=invalidEmail');
