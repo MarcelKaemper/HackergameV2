23404e65ae04b2aba153ed693bda6c53e10de877
Marcel <marcelkaemper862@gmail.com>
2019-04-26 21:19:54 +0200

    Added function to load the inbox

--- public/javascripts/functions/mail/loadInbox.js
+++ public/javascripts/functions/mail/loadInbox.js
@@ -1,16 +1,16 @@
 var query = require('../../database/dbquery.js');
 
-mailPreview = (uuid) => {
+loadInbox = (uuid) => {
     return new Promise(async (resolve, reject) => {
         let inbox = (await query("SELECT inbox FROM mails WHERE uuid='"+uuid+"';"))[0].inbox;
         inbox = inbox.replace(/(\r\n|\n|\r)/gm, "\\n");
         inbox = JSON.parse(inbox).mails;
         let previews = []
         for(let i = inbox.length-1; i>=0; i--){
-            previews.push({"sender":inbox[i].sender,"subject":inbox[i].subject})
+            previews.push({"sender":inbox[i].sender,"subject":inbox[i].subject, "message": inbox[i].message})
         }
         resolve(previews);
     });
 }
 
-module.exports = mailPreview;
+module.exports = loadInbox;
