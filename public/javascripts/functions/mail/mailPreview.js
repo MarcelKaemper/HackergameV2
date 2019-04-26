ab62ab598e6de61c72cd974e7230101829b90136
Marcel <marcelkaemper862@gmail.com>
2019-04-26 16:01:41 +0200

    Function to get mail previews

---
+++ public/javascripts/functions/mail/mailPreview.js
@@ -0,0 +1,20 @@
+var query = require('../../database/dbquery.js');
+
+mailPreview = (uuid) => {
+    return new Promise(async (resolve, reject) => {
+        let inbox = (await query("SELECT inbox FROM mails WHERE uuid='"+uuid+"';"))[0].inbox;
+        inbox = inbox.replace(/(\r\n|\n|\r)/gm, "\\n");
+        inbox = JSON.parse(inbox).mails;
+        console.log(inbox);
+        let previews = []
+        for(let x of inbox){
+            previews.push({"sender":x.sender,"subject":x.subject})
+        }
+        console.log(previews);
+        resolve(previews);
+    });
+}
+
+module.exports = mailPreview;
+
+// return [[subject1,subject2,subject3],[sender1,sender2,sender3]]
