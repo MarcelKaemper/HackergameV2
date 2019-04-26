dafcdd47cfb618ad177a71454e62801f06dbacc6
Marcel <marcelkaemper862@gmail.com>
2019-04-26 16:01:52 +0200

    Added function to send mails

--- public/javascripts/functions/mail/sendMail.js
+++ public/javascripts/functions/mail/sendMail.js
@@ -1,20 +1,23 @@
 var query = require('../../database/dbquery.js');
 
-function sendMail(req, sendTo, subject, message) {
-    return new Promise(async function(resolve, reject) {
+sendMail = (req, sendTo, subject, message) => {
+    return new Promise(async (resolve, reject) => {
         // Get UUID of receiver
-        let results = await query("SELECT uuid FROM userdata WHERE mail_address='"+sendTo+"';");
+        let results = await query("SELECT uuid FROM userdata WHERE mail_address='" + sendTo + "';");
         let sendToUuid = results[0].uuid;
 
         // Get inbox of receiver
-        results = await query("SELECT inbox FROM mails WHERE uuid='"+sendToUuid+"';");
-        let inbox = JSON.parse(results[0].inbox);
+        results = await query("SELECT inbox FROM mails WHERE uuid='" + sendToUuid + "';");
+        let inbox = results[0].inbox.replace(/(\r\n|\n|\r)/gm, "\\n");
+        inbox = JSON.parse(inbox);
+        console.log(inbox);
 
         // Add mail to inbox of receiver
-        inbox.mails.push(JSON.parse('{"sender":"'+req.session.mail+'", "subject":"'+subject+'", "message":"'+message+'"}'));
+        inbox.mails.push(JSON.parse('{"sender":"' + req.session.mail + '", "subject":"' + subject + '", "message":"' + message + '"}'));
         inbox = JSON.stringify(inbox);
-        await query("UPDATE mails SET inbox='"+inbox+"' WHERE uuid='"+sendToUuid+"';");
-
+        console.log(inbox);
+        await query("UPDATE mails SET inbox='" + inbox + "' WHERE uuid='" + sendToUuid + "';");
+        resolve();
     });
 }
 
