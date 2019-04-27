94e0a9dbc4c67022834ffcff27eacecc4c5dfa71
Marcel <marcelkaemper862@gmail.com>
2019-04-27 10:18:25 +0200

    Added deleteMail post listener

--- routes/mail.js
+++ routes/mail.js
@@ -4,17 +4,24 @@ var stdParameter = require('../public/javascripts/functions/stdParameter.js');
 var stdCall = require('../public/javascripts/functions/stdCall.js');
 var sendMail = require('../public/javascripts/functions/mail/sendMail.js');
 var loadInbox = require('../public/javascripts/functions/mail/loadInbox.js');
+var deleteMail = require('../public/javascripts/functions/mail/delMail.js');
 
 router.get('/', async (req, res, next) => {
     stdCall(req);
     let inbox = await loadInbox(req.session.uuid);
-    res.render('mail/mail', stdParameter(req, 'Mail', {preview: inbox}));
+    res.render('mail/mail', stdParameter(req, 'Mail', {preview: inbox.mails}));
 });
 
 router.post('/', async (req, res, next) => {
     stdCall(req);
     let inbox = await loadInbox(req.session.uuid);
     res.render('mail/mail', stdParameter(req, 'Mail', {preview: inbox, sender: req.body.sender, subject: req.body.subject, message: req.body.message}));
+});
+
+router.post('/deleteMail', async (req, res, next) => {
+    let inbox = await loadInbox(req.session.uuid);
+    await deleteMail(inbox, req.body.index, req.session.uuid);
+    res.redirect('/mail');
 });
 
 router.post('/sendMail', async (req, res, next) => {
