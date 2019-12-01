686291ce8f8cf9c8b83fbe31956fb61ddbafa662
Exarous <csincay@yahoo.com>
2019-12-01 21:47:11 +0100

    Fixed getVersion function

--- public/javascripts/functions/getVersion.js
+++ public/javascripts/functions/getVersion.js
@@ -1,11 +1,15 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+var query = require('../database/dbquery.js');
 
 const getVersion = () => {
-    const url = "https://api.github.com/repos/marcelkaemper/HackergameV2/releases";
-    return new Promise((resolve, reject) => {
-    	fetch(url)
-    	.then(res => res.json())
-    	.then(data => resolve(data[0]["name"]));
+    // const url = "https://api.github.com/repos/marcelkaemper/HackergameV2/releases";
+    return new Promise(async(resolve, reject) => {
+        var sql = "SELECT version FROM gameversion WHERE id=1;";
+        var result = await query(sql);
+    	// fetch(url)
+    	// .then(res => res.json())
+        // .then(data => resolve(data[0]["name"]));
+        resolve(result[0].version);
     });
 }
 
